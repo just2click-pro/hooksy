9cb1f7abdb57315a0eb3ee60a700e541b5189030
Dror a <just2click@outlook.com>
2022-01-23 21:41:56 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import Setup from "./tutorial/1-useState/setup/1-error-example"
+
+function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  )
+}
+
+export default App
