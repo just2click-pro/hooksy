dce9a89801350b4aca83da19848feb4e302ea949
Dror a <just2click@outlook.com>
2022-01-26 00:46:54 +0200

    Changing values with spread operator or separate useState

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 
 // import Setup from "./tutorial/1-useState/setup/1-error-example"
 // import Setup from "./tutorial/1-useState/setup/2-useState-basics"
-import Setup from "./tutorial/1-useState/setup/3-useState-array"
+// import Setup from "./tutorial/1-useState/setup/3-useState-array"
+import Setup from "./tutorial/1-useState/setup/4-useState-object"
 
 function App() {
   return (
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: "Mai",
+    age: 29,
+    message: "A true princes",
+  })
 
-export default UseStateObject;
+  const [name, setName] = useState("Mai")
+  const [age, setAge] = useState(29)
+  const [message, setMessage] = useState("A true princess")
+
+  const changeMessage = () => {
+    // const { message } = person
+    if (message.toLowerCase() === "a true princess") {
+      // setPerson({ ...person, message: "A street cat" })
+      setMessage("a street cat")
+    } else {
+      // setPerson({ ...person, message: "A true princess" })
+      setMessage("a true princess")
+    }
+  }
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h4>{age}</h4>
+      <h4>{message}</h4>
+
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
